de9e6f45680eedcbc3f92cda8fe45b0b634ac0f6
kimyeongseo <oseo0202@naver.com>
2021-05-11 18:16:46 +0900

    Fetching Movies from API And Rendering the Movies

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import './App.css';
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <>Hello</>
-  );
+//app을 mount하면 isLoading은 기본적으로 true이다.
+
+class App extends Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  // componentDidMount에서 data를 fetch(=불러온다.) 
+  // API로 부터 data fetching이 완료되면 movie를 Render하고 map을 만들고 movie를 render한다.
+
+  getMovies = async () => { //비동기 함수, 접근이 끝날 때까지 기다리도록 한다.
+    const { data: { data: { movies } } }
+      = await axios.get(" https://yts-proxy.now.sh/list_movies.json?sort_by=rating"); //axios가 끝날 때까지 기다렸다가 계속해
+    //this.setState({movies: movies}); 왼쪽은 state에 있고 오른쪽은 axios에 있는 data이다. 이렇게 양쪽이 같은 경우에는 생략이 가능하다.
+    this.setState({ movies, isLoading: false })
+  }
+
+  async componentDidMount() {
+    this.getMovies();
+  };
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : movies.map(movie => {
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            title={movie.title}
+            summary={movie.summary}
+            poster={movie.medium_cover_image} />
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
---
+++ src/Movie.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Movie({ is, year, title, summary, poster }) {
+    return (
+        <>
+            <h5>{title}</h5>
+        </>
+    );
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    year: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+
+}
+
+export default Movie;
